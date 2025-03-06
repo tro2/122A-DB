a0c59adfb11d38c269acb064457880152db4bdad
tro2 <62850247+tro2@users.noreply.github.com>
2025-03-06 13:47:05 -0800

    created framework for command handling

--- src/project.py
+++ src/project.py
@@ -1,5 +1,43 @@
 import mysql.connector
+import sys
+import load
 
-mysql.connector.connect(user='test', password='password', database='cs122a')
+# Open connection
+conn = mysql.connector.connect(user='test', password='password', database='cs122a')
+cur = conn.cursor()
 
-print("Connected")
+# Arg processing
+command, *args = sys.argv[1:]
+
+match command:
+    case 'import':
+        load.exec(cur)
+        print('import')
+    case 'insertViewer':
+        print('insertViewer')
+    case 'addGenre':
+        print('addGenre')
+    case 'deleteViewer':
+        print('deleteviewer')
+    case 'insertMovie':
+        print('insertMovie')
+    case 'insertSession':
+        print('insertSession')
+    case 'updateRelease':
+        print('updateRelease')
+    case 'listReleases':
+        print('listReleases')
+    case 'popularRelease':
+        print('popularRelease')
+    case 'releaseTitle':
+        print('releaseTitle')
+    case 'activeViewers':
+        print('activeViewers')
+    case 'videosViewed':
+        print('videosViewed')
+    case _:
+        print("Error, unrecognized command")
+
+# Clean up connection
+cur.close()
+conn.close()
